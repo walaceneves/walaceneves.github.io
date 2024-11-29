e1d012e690a816e5c12ca5513f08a6ac108ada67
walaceneves <105088320+walaceneves@users.noreply.github.com>
2024-11-29 20:31:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Site de Astronomia carregado com sucesso!");
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("modo-tema");
+
+    button.addEventListener("click", () => {
+        // Alterna a classe "claro" no body
+        document.body.classList.toggle("claro");
+        document.querySelector("header").classList.toggle("claro");
+        document.querySelector("footer").classList.toggle("claro");
+
+        // Atualiza o texto do botão
+        if (document.body.classList.contains("claro")) {
+            button.textContent = "Modo Escuro";
+        } else {
+            button.textContent = "Modo Claro";
+        }
+    });
+});
